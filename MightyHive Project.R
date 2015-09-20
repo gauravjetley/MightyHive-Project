4e6092c4e34731d7f8aea5129be2c88538fe6a25
Gaurav Jetley <gauravjetley@gmail.com>
2015-09-20 17:17:06 -0400

    Cleaned both datasets

--- MightyHive Project.R
+++ MightyHive Project.R
@@ -69,8 +69,13 @@ reservation_match
 write.csv(x = reservation_match, file = "reservation_match.csv")
 write.csv(x = abandoned_match, file = "abandoned_match.csv")
 
+
+
+
 ####Cleaning Matched Datasets
 
+
+# Reservation_match Data set cleaning
 # Crosschecking for any errors
 sum(match(reservation_match$Email, abandoned_match$Email, nomatch = 0) | 
   match(reservation_match$Incoming_Phone, abandoned_match$Incoming_Phone, nomatch = 0) |
@@ -88,16 +93,36 @@ duplicated(reservation_match$Contact_Phone,incomparables = "")
 sum(duplicated(reservation_match$Contact_Phone, incomparables = "")) # 0 obs with >1 reservation from same Contact Phone
 
 duplicated(reservation_match$Email,incomparables = "")
-sum(duplicated(reservation_match$Email, incomparables = ""))
+sum(duplicated(reservation_match$Email, incomparables = "")) # 5 obs
 
-# Removing >1 reservation obs from reservation_match dataset
+# Removing >1 reservation (incoming) obs from reservation_match dataset
 reservation_match_oneres = reservation_match[!duplicated(reservation_match$Incoming_Phone, incomparables = ""),]
+nrow(reservation_match_oneres) #same as abandoned_match
 
+# Checking again for duplicate emails 
 duplicated(reservation_match_oneres$Email,incomparables = "")
 sum(duplicated(reservation_match_oneres$Email, incomparables = ""))
 
+# Removing >1 reservation (Email) obs from reservation_match dataset
+reservation_match_oneres <- reservation_match_oneres[!duplicated(reservation_match_oneres$Email, incomparables = ""),]
+nrow(reservation_match_oneres) # one less than abandoned_match
 
 # Writing new reservation_match_oneres dataset
 write.csv(x = reservation_match, file = "reservation_match_oneres.csv")
 
 
+#Abandoned_match dataset cleaning
+#Finding duplicates
+sum(duplicated(abandoned_match$Incoming_Phone, incomparables = ""))
+sum(duplicated(abandoned_match$Contact_Phone, incomparables = "")) # 1 dublicate Contact Phone
+sum(duplicated(abandoned_match$Email, incomparables = ""))
+
+# Removing duplicates
+abandoned_match[abandoned_match$Contact_Phone=="(207)-726-7898",]
+abandoned_match[duplicated(abandoned_match$Contact_Phone, incomparables = ""),] # making sure that 8168 is older observation of the two
+
+abandoned_match_nodup <- abandoned_match[!duplicated(abandoned_match$Contact_Phone, incomparables = ""),]
+nrow(abandoned_match_nodup) # Same as reservation_match_oneres
+
+# Writing new abandoned_match_nodup data set
+write.csv(x = reservation_match, file = "abandoned_match_nodup.csv")
